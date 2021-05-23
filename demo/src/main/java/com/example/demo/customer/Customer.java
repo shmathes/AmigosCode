e22d7389c33da8023ebc7245fb54f1ec9153f007
Shawn <shawnmathes8807@gmail.com>
2021-05-23 13:28:33 -0400

    Jackson JSON annotations.

--- demo/src/main/java/com/example/demo/customer/Customer.java
+++ demo/src/main/java/com/example/demo/customer/Customer.java
@@ -1,15 +1,21 @@
 package com.example.demo.customer;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Customer
 {
     private final Long id;
     private final String name;
+    private final String password;
 
-    Customer(Long id, String name){
+    Customer(Long id, String name, String password){
         this.id = id;
         this.name = name;
+        this.password = password;
     }
 
+    @JsonProperty("customerId") //Change the property name from id to customerId
     public Long getId() {
         return id;
     }
@@ -18,11 +24,19 @@ public class Customer
         return name;
     }
 
+    @JsonIgnore //Prevents property from being sent back during the request
+    public String getPassword(){return password;}
+
+    //customerId
+//    public Long getCustomerId(){ return id;}
+
+
     @Override
     public String toString() {
         return "Customer{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 }
