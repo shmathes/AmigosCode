16eea7767f928c5828c369498a0e5fcfad637786
Shawn <shawnmathes8807@gmail.com>
2021-05-23 19:27:27 -0400

    Post request.

--- demo/src/main/java/com/example/demo/customer/CustomerController.java
+++ demo/src/main/java/com/example/demo/customer/CustomerController.java
@@ -1,7 +1,6 @@
 package com.example.demo.customer;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,21 @@ public class CustomerController
     List<Customer> getCustomer()
     {
         return customerService.getCustomer();
+    }
+    @PostMapping
+    void createNewCustomer(@RequestBody Customer customer){
+        System.out.println("POST REQUEST");
+        System.out.println(customer);
+    }
+
+    @DeleteMapping(path = "{customerId}")
+    void deleteCustomer(@PathVariable("customerId") Long id){
+        System.out.println("DELETE REQUEST for customer with id " + id);
+    }
+
+    @PutMapping
+    void updateCustomer(@RequestBody Customer customer){
+        System.out.println("PUT REQUEST");
+        System.out.println(customer);
     }
 }
